d6812206559858d86c1320aa399f34d0e11ee77b
Silvan Hess <silvan.hess@stud.unilu.ch>
2024-05-27 19:44:24 +0200

    prepared the filters cantons and tags for the shiny app

--- scripts/02_Prepare dataframe for shiny app.R
+++ scripts/02_Prepare dataframe for shiny app.R
@@ -1,13 +1,43 @@
 # load preprocessed data ----
 
-#clear work space
+# clear work space
 rm(list = ls())
 
-#load packages
+# load packages
 library(tidyverse)
 
-#load the data
+# load the data
 load("data_prep/swiss_voting.RData")
 
 # prepare data for shiny app ----
 
+# overview
+glimpse(df)
+
+## cantons ----
+
+# delete missing values in cantons
+df <- df %>% filter(!is.na(canton))
+
+# create a list of all possible cantons
+cantons <- df$canton %>% unique()
+
+# sort alphabetically
+cantons <- sort(cantons)
+
+#print out object type of cantons
+class(cantons)
+
+## tags ----
+
+# create a list of all possible tags
+tags <- df$tags %>% unique()
+
+# create a character vector of all tags
+tags <- unlist(tags)
+
+# delete double entries
+tags <- unique(tags)
+
+# sort alphabetically
+tags <- sort(tags)
