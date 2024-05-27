11433862a608517eefe09d8116d84a0252051f96
Silvan Hess <silvan.hess@stud.unilu.ch>
2024-05-27 19:24:14 +0200

    create new script to prepare df for shiny app

---
+++ scripts/02_Prepare dataframe for shiny app.R
@@ -0,0 +1,13 @@
+# load preprocessed data ----
+
+#clear work space
+rm(list = ls())
+
+#load packages
+library(tidyverse)
+
+#load the data
+load("data_prep/swiss_voting.RData")
+
+# prepare data for shiny app ----
+
