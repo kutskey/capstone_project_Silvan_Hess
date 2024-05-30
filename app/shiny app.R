45f0a6bd9ceff42ed753bb4ed8827ffad86d49f1
Silvan Hess <silvan.hess@stud.unilu.ch>
2024-05-30 11:56:35 +0200

    added feature: display grouped table by canton

--- app/shiny app.R
+++ app/shiny app.R
@@ -4,55 +4,43 @@
 library(shiny)
 library(bslib)
 
-# Modeling
-library(modeldata)
-library(DataExplorer)
-
-# Widgets
-library(plotly)
-
 # Core
 library(tidyverse)
 
 # LOAD DATASETS ----
 
-# insert script 01_Preprocess data here
+load("data_prep/app_data.RData")
 
 # 1.0 USER INTERFACE ----
 
 ui <- navbarPage(
- 
   title = "Cantonal Voting",
   
   theme = bslib::bs_theme(
     version = 4,
-    bootswatch = "minty",
+    bootswatch = "minty"
   ),
   
   tabPanel(
     title = "Explore",
-           sidebarLayout(
-             
-             sidebarPanel(
-               width = 3,
-               h1("Data Explorer"),
-               
-               selectInput(
-                 inputId = "", 
-                 label = "Choose a dataset", 
-                 choices = c("iris", "mtcars")),
-               uiOutput("variable"),
-               uiOutput("plot")
-             ), # input tbd (two filters, canton and topic)
-             
-             mainPanel(
-               h3("Data Summary"),
-               verbatimTextOutput("summary"),
-               h3("Data Structure"),
-               verbatimTextOutput("structure")
-             ) # output tbd (table)
-           )
-  ),
+    sidebarLayout(
+      sidebarPanel(
+        width = 3,
+        h1("Data Explorer"),
+        
+        selectInput(
+          inputId = "selected_canton", 
+          label = "Select Canton:", 
+          choices = unique(app_table$canton)
+        )
+      ),
+      
+      mainPanel(
+        h3("Table Summary"),
+        tableOutput("canton_table")
+      )
+    )
+  )
 )
 
 
@@ -61,21 +49,13 @@ ui <- navbarPage(
 
 server <- function(input, output) {
   
-  rv <- reactiveValues()
-  
-  observe({
-    
-    rv$dataset <- data_list %>% pluck(input$dataset_choice)
+  filtered_data <- reactive({
+    app_table[app_table$canton == input$selected_canton, ]
   })
   
-  output$variable <- renderUI({
-    
-    selectInput(
-      inputId = "variable_choice",
-      label = "Choose a variable",
-      choices = names(rv$dataset),
-      selected = names(rv$dataset)[1]
-    ) # output tbd (table)
+  # Render the filtered data as a table
+  output$canton_table <- renderTable({
+    filtered_data()
   })
   
 }
